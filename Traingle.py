f80799e986d51483119dffd108b49b1c67395f35
chitrashalini <109006879+chitrashalini@users.noreply.github.com>
2022-07-09 22:08:40 -0700

    python program

---
+++ Traingle.py
@@ -0,0 +1,7 @@
+for i in range(1,6):
+    if i > 1:
+        for j in range(i + 1):
+             print(j,end ="")
+   if i < 6:
+     continue
+            print(i)
---
+++ data stru.py
@@ -0,0 +1,38 @@
+Microsoft Windows [Version 10.0.17134.1304]
+(c) 2018 Microsoft Corporation. All rights reserved.
+
+C:\Users\Lenovo>python
+Python 3.7.1 (v3.7.1:260ec2c36a, Oct 20 2018, 14:57:15) [MSC v.1915 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+>>> '''list'''
+'list'
+>>> 3,1
+(3, 1)
+>>> 3.1
+3.1
+>>> users =['Admistrator','ServerAdmin','NetworkAdmin']
+>>> print(users[0])
+Admistrator
+>>> print(users[2])
+NetworkAdmin
+>>> print(users[1])
+ServerAdmin
+>>> '''tuples'''
+'tuples'
+>>> users = ('Adminisrator','ServerAdmin','NetworkAdmin',)
+>>> print(users[0])
+Adminisrator
+>>> print(users[1])
+ServerAdmin
+>>> print(users[2])
+NetworkAdmin
+>>> '''dic'''
+'dic'
+>>> users = {'u1':'Administrator','u2':'ServerAdmin','u3':'NetworkAdmin'}
+>>> print(users['u1'])
+Administrator
+>>> print(users['u2'])
+ServerAdmin
+>>> print(users['u3'])
+NetworkAdmin
+>>>
